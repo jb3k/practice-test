347a70266affa5ac73c7904a6061cecf654e3445
JB Kam <jb3.kam@gmail.com>
2023-03-06 15:09:33 -0800

    updated main init file

--- aA_Mock_TakeHomeChallenge-master/backend_challenge/app/__init__.py
+++ aA_Mock_TakeHomeChallenge-master/backend_challenge/app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_migrate import Migrate
+
+from app.models import db
 from app.config import Config
+from app.api.coffee_routes import coffee_routes
+from app.api.post_routes import post_routes
 
 
 app = Flask(__name__)
@@ -7,8 +12,8 @@ app.config.from_object(Config)
 
 # @app.route('/')
 
-app.register_blueprint()
-
+app.register_blueprint(coffee_routes, url_prefix='/api/coffee')
+app.register_blueprint(post_routes, url_prefix='/api/post')
 db.init_app(app)
 Migrate(app, db)    
 
--- aA_Mock_TakeHomeChallenge-master/backend_challenge/app/models/__init__.py
+++ aA_Mock_TakeHomeChallenge-master/backend_challenge/app/models/__init__.py
@@ -1,2 +1,5 @@
 from .coffee import Coffee
 from .post import Post
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
